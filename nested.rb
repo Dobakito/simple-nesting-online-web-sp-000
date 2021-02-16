0eebd68c6ea92e3e70cb71df3874b291c91f6266
Michael Dobak <mdobak44@icloud.com>
2021-02-16 00:09:08 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -98,7 +98,7 @@ def changing_alan
  		}
     
      programmer_hash[:alan_kay][:known_for] = "GUI"
-     programmer_hash
+     
      
 end
 
@@ -118,6 +118,6 @@ def adding_to_dennis
           :languages => ["C"]
         }
      }
-  programmer_hash[:dennis_ritchie][:languages] << "Assembly"
+  programmer_hash[:dennis_ritchie][:languages] = "Assembly"
   programmer_hash
 end
